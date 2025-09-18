aebf1ae18a2e352a5385adfb3b91d66d1e49bfc1
AlixNelson <2664732230@qq.com>
2025-09-18 23:15:20 +0800

    重构了对多边形的初始化逻辑

--- physics.c
+++ physics.c
@@ -7,7 +7,7 @@
 typedef struct {
     double x;
     double y; 
-} vector; // 用一个结构体定义二维数组，可以是向量或坐标
+} vector; // 用一个结构体定义，可以是向量或坐标
 typedef struct {
     vector start;
     vector end;
@@ -17,9 +17,13 @@ typedef struct {
     int point_count; // 顶点的数量
     vector position; // 重心
     float mass; // 质量
-    vector v; // 速度
+    vector v; // 线速度
     vector F; // 合力
-} object; // 实体，由许多顶点定义，运算时两个顶点被解为一个线段，由线段表达式是否相交判断碰撞
+    float ang_v; // 角速度
+    float torque; // 扭矩
+    float inertia; // 转动惯量
+    // 我不会角动量定理，但先定义再说
+} object; // 实体，由许多顶点定义，运算时两个顶点被解为一个线段，由线段表达式是否相交来判断碰撞
 
 // 定义向量对向量运算
 vector add_vv(vector a, vector b) { // 加法
@@ -76,24 +80,31 @@ vector divide_vd(vector a, double b) {
 }
 // 其他暂时不定义，估计不会用到
 
-object c = {0};
+object car = {0};
 
-void init() {
-    c = (object) {
-        .mass = 1000,
-        .position = {
-            .x = 0,
-            .y = 0
-        },
-        .v = {
-            .x = 0,
-            .y = 0,
-        },
-        .F = {
-            .x = 200,
-            .y = 300,
-        }
+vector square[4] = { // 这是一个正方体的原型
+    {-1,-1},{-1,1},{1,1},{1,-1}
+};
+
+vector* init_polygonal/*初始化多边形*/(vector *points/*顶点数组*/, int count, float times /*倍率*/) {
+    vector* new_points = malloc(sizeof(vector) * count);
+    for (int i = 0; i < count; ++i) {
+        new_points[i] = mut_vd(points[i], times);
     };
+}
+
+void init(object *car) {
+    car->point = init_polygonal(square, 4, 2);
+    car->point_count = 4;
+    car->mass = 1000;
+    car->position.x = 0;
+    car->position.y = 0;
+    car->F.x = 200;
+    car->F.y = 300;
+    /*
+    car = (object) {.mass = 1000,.position = {.x = 0,.y = 0},.v = {.x = 0,.y = 0,},.F = {.x = 200,.y = 300,}};
+    没用的东西，这是没有线条时的初始化
+    */
 }
 
 void newton(object *c, float t) {  // 牛顿第二定律
@@ -106,14 +117,21 @@ void newton(object *c, float t) {  // 牛顿第二定律
     c->v = add_vv(c->v, accelerate);
 }
 
-void throw() {}
+void vectors(object obj) { 
+/*
+将多边形的顶点向量化 可是真的需要这么做吗？
+或许可以直接根据点的坐标判断？
+这两个路线我都没有认真考虑过
+*/
+}
+
 
 int main(void) {
-    printf("%f,%f\n", c.v.x, c.v.y);
-    init();
-    printf("%f,%f\n", c.v.x, c.v.y);
-    newton(&c, 2);
-    printf("%f,%f\n", c.v.x, c.v.y);
+    printf("%f,%f\n", car.v.x, car.v.y);
+    init(&car);
+    printf("%f,%f\n", car.v.x, car.v.y);
+    newton(&car, 2);
+    printf("%f,%f\n", car.v.x, car.v.y);
     system("pause");
     return 0;
 }
